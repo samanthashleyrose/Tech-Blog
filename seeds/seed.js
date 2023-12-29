f8f8023fc5c0492bc5dca66d5efe3ebdf0e4ee4c
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-29 17:13:08 -0500

    Began boiler plate code for seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,19 @@
 const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
