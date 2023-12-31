9f0ea0c39fa282f947255c072f5b4a631306d78c
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-31 14:42:21 -0500

    Adjusted get route for root URL - LOGIN

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 
-// get route for the root URL
+// get route for the root URL - LOGIN
 router.get('/', (req, res) => {
+    // if user is already logged in, redirect to homepage
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+      }
   res.render('login'); 
 });
 
