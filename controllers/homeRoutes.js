102227b46cd4a3b89384ea54d96a1ac991531353
samanthashleyrose <samantha.rose327@gmail.com>
2024-01-01 13:22:40 -0500

    Added withAuth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const path = require('path');
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // get route for the root URL - LOGIN
 router.get('/', (req, res) => {
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 // get route that directs user to profile page after sign up button is selected on the login screen
-router.get('/profile', (req, res) => {
+router.get('/profile', withAuth, (req, res) => {
   // if user is already logged in, redirect to homepage
   if (req.session.logged_in) {
       res.redirect('/homepage');
