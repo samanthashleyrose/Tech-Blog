561332674979b152cb444022123b8afc8c1f7f0c
samanthashleyrose <samantha.rose327@gmail.com>
2024-01-01 13:27:34 -0500

    Reorganized userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,18 +2,31 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // post route to create a new user 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const { email, password } = req.body;
+
+        // Password criteria check
+        if (password.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+            return res.render('login', {
+                errorMessage: 'Password must be at least 8 characters long and contain 1 special character.',
+            });
+        }
+
+        const userData = await User.create({
+            email,
+            password,
+        });
 
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
 
-            res.status(200).json(userData);
+            res.redirect('/profile'); // Redirect to the profile page on successful signup
         });
     } catch (err) {
-        res.status(400).json(err);
+        // Display error message on the login page
+        res.render('login', { errorMessage: 'Error creating user. Please try again.' });
     }
 });
 
@@ -47,34 +60,6 @@ router.post('/login', async (req, res) => {
 
     } catch (err) {
         res.status(400).json(err);
-    }
-});
-
-router.post('/signup', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Password criteria check
-        if (password.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-            return res.render('login', {
-                errorMessage: 'Password must be at least 8 characters long and contain 1 special character.',
-            });
-        }
-
-        const userData = await User.create({
-            email,
-            password,
-        });
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.redirect('/profile'); // Redirect to the profile page on successful signup
-        });
-    } catch (err) {
-        // Display error message on the login page
-        res.render('login', { errorMessage: 'Error creating user. Please try again.' });
     }
 });
 
